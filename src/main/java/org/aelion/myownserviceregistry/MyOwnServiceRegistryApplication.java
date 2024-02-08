85b2c5b4ce5a91d55d9bf3284f98886568a761ad
Ismail BASSAOUD <ismail.bassaoud@gmail.com>
2024-02-08 10:59:53 +0100

    :tada: Initial commit

---
+++ src/main/java/org/aelion/myownserviceregistry/MyOwnServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package org.aelion.myownserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOwnServiceRegistryApplication.class, args);
+	}
+
+}
